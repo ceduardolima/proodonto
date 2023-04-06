83aa5e367228b3debed5eab0f610e75e5ff46ec5
Eduardo <ceduardolima2000@gmail.com>
2023-04-06 00:14:02 -0300

    Criando esqueleto da tela de login

---
+++ proodonto/lib/ui/login/login.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/default_size.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            body: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: PaddingSize.big),
+              child: LoginTextForm(),
+    )));
+  }
+}
+
+class LoginTextForm extends StatelessWidget {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  LoginTextForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Text(
+          "Login",
+          style: TextStyle(
+            fontSize: 32.0,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        LoginTextField(
+          emailController,
+          "Email",
+          TextInputType.emailAddress,
+          hint: "xxxx@upe.com.br",
+          icon: const Icon(Icons.email_outlined),
+        ),
+        LoginTextField(
+          passwordController,
+          "Senha",
+          TextInputType.text,
+          isPassword: true,
+          icon: const Icon(Icons.password),
+        ),
+        const LoginButton()
+      ],
+    );
+  }
+}
+
+class LoginTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String? hint;
+  final String label;
+  final Icon? icon;
+  final TextInputType inputType;
+  bool? isPassword = false;
+  final String validatorMessenger = "O campo é obrigatório";
+
+  LoginTextField(this.controller, this.label, this.inputType,
+      {this.hint, this.icon, this.isPassword, Key? key})
+      : super(key: key);
+
+  String? formValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return validatorMessenger;
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: isPassword ?? false,
+      controller: controller,
+      keyboardType: inputType,
+      decoration: InputDecoration(icon: icon, labelText: label, hintText: hint),
+      validator: (String? value) => validatorMessenger,
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: PaddingSize.medium),
+      child: ElevatedButton(
+          onPressed: () => {},
+          style: ElevatedButton.styleFrom(
+            alignment: Alignment.center,
+            minimumSize: const Size.fromHeight(50),
+          ),
+          child: const Text(
+            "Login",
+            style: TextStyle(fontSize: 16),
+          )),
+    );
+  }
+}
---
+++ proodonto/lib/utils/default_size.dart
@@ -0,0 +1,5 @@
+class PaddingSize {
+  static const double small = 8.0;
+  static const double medium = 16.0;
+  static const double big = 24.0;
+}
