9729eb9c206f3bed7d53ecd7000feebe51d341d2
Eduardo <ceduardolima2000@gmail.com>
2023-05-02 08:47:56 -0300

    Criando botão padrão

---
+++ proodonto/lib/widget/buttons.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../utils/default_size.dart';
+
+class DefaultButton extends StatelessWidget {
+  const DefaultButton({Key? key, required this.text, required this.onPressed, this.alignment, this.minimumSize}) : super(key: key);
+  final Function onPressed;
+  final Alignment? alignment;
+  final double? minimumSize;
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: PaddingSize.medium),
+      child: ElevatedButton(
+        onPressed: () => onPressed,
+        style: ElevatedButton.styleFrom(
+          alignment: alignment ?? Alignment.center,
+          minimumSize: Size.fromHeight(minimumSize ?? 50),
+        ),
+        child: Text(text),
+      ),
+    );
+  }
+}
+
