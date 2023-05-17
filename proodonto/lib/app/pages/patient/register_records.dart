abeac7b708491ed9de2234564799e276350e8710
Eduardo <ceduardolima2000@gmail.com>
2023-05-17 20:06:30 -0300

    feature: Criando validação dos campos do cadastro do prontuário

--- proodonto/lib/app/pages/patient/register_records.dart
+++ proodonto/lib/app/pages/patient/register_records.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:proodonto/app/database/database.dart';
 import 'package:proodonto/app/pages/patient/register_patient_informations.dart';
-import 'package:proodonto/app/repository/patient_repository.dart';
 import 'package:proodonto/app/shared/default_size.dart';
 import 'package:proodonto/app/widget/buttons.dart';
-
+import 'package:form_builder_validators/form_builder_validators.dart';
 import '../../database/entity/patient.dart';
 
 class RegisterRecordsHome extends StatelessWidget {
@@ -32,37 +31,62 @@ class RegisterRecordsHome extends StatelessWidget {
   }
 }
 
-class _RegisterRecordsForm extends StatelessWidget {
+class _RegisterRecordsForm extends StatefulWidget {
   _RegisterRecordsForm({Key? key, required this.database}) : super(key: key);
+  final ProodontoDatabase database;
+  final patient = Patient();
+
+  @override
+  State<_RegisterRecordsForm> createState() =>
+      _RegisterRecordsFormState(database: database);
+}
+
+class _RegisterRecordsFormState extends State<_RegisterRecordsForm> {
+  _RegisterRecordsFormState({required this.database});
+
   final ProodontoDatabase database;
   final _formKey = GlobalKey<FormBuilderState>();
   final patient = Patient();
+  String? errorMessenger;
+
   @override
   Widget build(BuildContext context) {
     return FormBuilder(
         key: _formKey,
+        autovalidateMode: AutovalidateMode.disabled,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const _RecordFormField(
+            _RecordFormField(
               name: "recordNumber",
               label: "Número do prontuário",
               inputType: TextInputType.number,
             ),
-            const _RecordFormField(name: "advisor", label: "Orientador"),
-            const _RecordFormField(
+            _RecordFormField(
+              name: "advisor",
+              label: "Orientador",
+            ),
+            _RecordFormField(
               name: "semester",
               label: "Período",
               inputType: TextInputType.number,
             ),
-            const _RecordFormField(
-                name: "careUnit", label: "Unidade de atendimento"),
-            const _RecordFormField(name: "initialExam", label: "Exame inicial"),
+            _RecordFormField(
+              name: "careUnit",
+              label: "Unidade de atendimento",
+            ),
+            _RecordFormField(
+              name: "initialExam",
+              label: "Exame inicial",
+            ),
             DefaultButton(
               onPressed: () {
-                getFields();
-                _changeToPatientRegister(context, patient);
+                bool isValid = _validateItems();
+                if (isValid) {
+                  getFields();
+                  _changeToPatientRegister(context, patient);
+                }
               },
               text: "Próximo",
             )
@@ -70,13 +94,26 @@ class _RegisterRecordsForm extends StatelessWidget {
         ));
   }
 
+  bool _validateItems() {
+    final values = _formKey.currentState?.fields.values.toList();
+    bool isValid = true;
+    values?.forEach((element) {
+      element.validate();
+      if (!element.isValid) {
+        isValid = false;
+      }
+    });
+    return isValid;
+  }
+
   void getFields() {
+    _formKey.currentState?.save();
     var fields = _formKey.currentState?.fields;
-      patient.recordNumber = int.parse(fields!["recordNumber"]?.value);
-      patient.advisor = fields["advisor"]?.value;
-      patient.semester = fields["semester"]?.value;
-      patient.careUnit = fields["careUnit"]?.value;
-      patient.initialExam = fields["initialExam"]?.value;
+    patient.recordNumber = int.parse(fields!["recordNumber"]?.value);
+    patient.advisor = fields["advisor"]?.value;
+    patient.semester = fields["semester"]?.value;
+    patient.careUnit = fields["careUnit"]?.value;
+    patient.initialExam = fields["initialExam"]?.value;
   }
 
   void _changeToPatientRegister(BuildContext context, Patient patient) {
@@ -91,18 +128,22 @@ class _RegisterRecordsForm extends StatelessWidget {
 }
 
 class _RecordFormField extends StatelessWidget {
-  const _RecordFormField(
+  _RecordFormField(
       {required this.name,
       required this.label,
       this.hint,
       Key? key,
-      this.inputType})
+      this.inputType,
+      this.maxLength,
+      this.errorMessenger})
       : super(key: key);
 
   final String name;
   final String? hint;
   final String label;
+  final int? maxLength;
   final TextInputType? inputType;
+  String? errorMessenger;
 
   @override
   Widget build(BuildContext context) {
@@ -111,8 +152,19 @@ class _RecordFormField extends StatelessWidget {
       child: FormBuilderTextField(
         name: name,
         keyboardType: inputType,
-        decoration: InputDecoration(labelText: label, hintText: hint),
+        decoration: InputDecoration(
+            labelText: label, hintText: hint,),
+        maxLines: 1,
+        maxLength: maxLength,
+        validator: (value) => _validate(value),
       ),
     );
+  }
+
+  String? _validate(String? value) {
+    if (value == null || value.isEmpty) {
+      return errorMessenger ?? "Campo obrigatório";
+    }
+    return null;
   }
 }
