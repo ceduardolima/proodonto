fbd43adc292f8be3ff963814c100ac8ecdd2d6ca
Eduardo <ceduardolima2000@gmail.com>
2023-05-22 20:03:57 -0300

    Criando o triage dao

---
+++ proodonto/lib/app/database/dao/triage_dao.dart
@@ -0,0 +1,27 @@
+import 'package:floor/floor.dart';
+import 'package:proodonto/app/database/entity/triage.dart';
+
+@dao
+abstract class TriageDao {
+
+  @Query("SELECT * FROM triage")
+  Future<List<Triage>> getAll();
+
+  @Query("SELECT * FROM triage WHERE id=:id")
+  Stream<Triage?> findById(int id);
+
+  @Query("SELECT * FROM triage WHERE recordNumber=:recordNumber")
+  Stream<Triage?> findByRecordNumber(int recordNumber);
+
+  @Query("SELECT * FROM triage WHERE patientCPF=:cpf")
+  Stream<List<Triage?>> findByPatientCPF(String cpf);
+
+  @Query("SELECT * FROM triage WHERE operatorCPF=:cpf")
+  Stream<List<Triage?>> findByOperatorCPF(String cpf);
+
+  @Insert(onConflict: OnConflictStrategy.fail)
+  Future<void> insert(Triage triage);
+
+  @Query("DELETE FROM triage WHERE id=:id")
+  Future<void> deletePatientByCPF(int id);
+}
