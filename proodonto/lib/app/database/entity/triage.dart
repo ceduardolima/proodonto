1fe969054d7cedf4652a5b7fa6af1e320a9408e5
Eduardo <ceduardolima2000@gmail.com>
2023-05-22 19:55:10 -0300

    Criando a entidade Triage

---
+++ proodonto/lib/app/database/entity/triage.dart
@@ -0,0 +1,52 @@
+import 'dart:core';
+
+import 'package:floor/floor.dart';
+
+@Entity(tableName: "triage")
+class Triage {
+  int? id;
+  int? patientCPF;
+  int? operatorCPF;
+  String? operatorName;
+  String? patientName;
+  int? recordNumber;
+  String? reasonForConsultation;
+  bool? hasCovid;
+  bool? hasCough;
+  String? testType;
+  String? kinship;
+  bool? hasFever;
+  bool? hasDifficultyToBreathing;
+  bool? hasTiredness;
+  bool? hasLossOfSmell;
+  bool? hasLossOfTaste;
+  bool? hasSoreThroat;
+  bool? hasHeadache;
+  bool? hasDiarrhea;
+  String? oximetry;
+  String? heartRate;
+  String? temperature;
+
+  Triage(
+      {this.id,
+      this.patientCPF,
+      this.operatorCPF,
+      this.operatorName,
+      this.patientName,
+      this.recordNumber,
+      this.reasonForConsultation,
+      this.hasCovid,
+      this.testType,
+      this.kinship,
+      this.hasFever,
+      this.hasDifficultyToBreathing,
+      this.hasTiredness,
+      this.hasLossOfSmell,
+      this.hasLossOfTaste,
+      this.hasSoreThroat,
+      this.hasHeadache,
+      this.hasDiarrhea,
+      this.oximetry,
+      this.heartRate,
+      this.temperature});
+}
