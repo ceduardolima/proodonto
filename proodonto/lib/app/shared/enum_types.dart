b7d9d7ce21511922961c276eeee40d3a00303405
Eduardo <ceduardolima2000@gmail.com>
2023-05-24 19:26:54 -0300

    Criando tipos enumerados do exam

--- proodonto/lib/app/shared/enum_types.dart
+++ proodonto/lib/app/shared/enum_types.dart
@@ -52,3 +52,101 @@ enum Sex {
     return getNameList()[index];
   }
 }
+
+enum GeneralType {
+  good,
+  medium,
+  bad;
+
+  static List<String> getNameList() => ["Bom", "Regular", "Ruim"];
+
+  static String getValueByName(GeneralType generalType) {
+    const nameList = GeneralType.values;
+    int index = nameList.indexOf(generalType);
+    return getNameList()[index];
+  }
+}
+
+enum Surface {
+  smooth,
+  rough;
+
+  static List<String> getNameList() => ["Lisa", "Rugosa"];
+
+  static String getValueByName(Surface surface) {
+    const nameList = Surface.values;
+    int index = nameList.indexOf(surface);
+    return getNameList()[index];
+  }
+}
+
+enum Sensibility {
+  absent,
+  present;
+
+  static List<String> getNameList () => ["Ausênte", "Presente"];
+
+  static String getValueByName(Sensibility sensibility) {
+    const nameList = Sensibility.values;
+    int index = nameList.indexOf(sensibility);
+    return getNameList()[index];
+  }
+}
+
+enum Asymmetry {
+  absent,
+  present;
+
+  static List<String> getNameList () => ["Ausênte", "Presente"];
+
+  static String getValueByName(Asymmetry asymmetry) {
+    const nameList = Asymmetry.values;
+    int index = nameList.indexOf(asymmetry);
+    return getNameList()[index];
+  }
+}
+
+enum Mobility {
+  absent,
+  present;
+
+  static List<String> getNameList () => ["Ausênte", "Presente"];
+
+  static String getValueByName(Mobility mobility) {
+    const nameList = Mobility.values;
+    int index = nameList.indexOf(mobility);
+    return getNameList()[index];
+  }
+}
+
+enum Lip {
+  skin,
+  semiMucous,
+  lipCommissure,
+  mucous;
+
+  static List<String> getNameList () => ["Pele", "Semi mucosa", "Comissura labial", "Mucosa"];
+
+  static String getValueByName(Lip lip) {
+    const nameList = Lip.values;
+    int index = nameList.indexOf(lip);
+    return getNameList()[index];
+  }
+}
+
+enum Tongue {
+  backFace,
+  ventral,
+  edges,
+  apex;
+
+  static List<String> getNameList () => ["Face dorsal", "Ventral", "Bordas", "Ápice"];
+
+  static String getValueByName(Tongue tongue) {
+    const nameList = Tongue.values;
+    int index = nameList.indexOf(tongue);
+    return getNameList()[index];
+  }
+
+
+}
