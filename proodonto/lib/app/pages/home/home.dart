2efd0973be2fc1e4b0717c58cb56a87848845d64
Eduardo <ceduardolima2000@gmail.com>
2023-07-29 10:56:36 -0300

    feat: Criando nova página para consultar informações do paciênte

--- proodonto/lib/app/pages/home/home.dart
+++ proodonto/lib/app/pages/home/home.dart
@@ -5,13 +5,14 @@ import 'package:proodonto/app/database/database.dart';
 import 'package:proodonto/app/pages/anamnesis/anamnesis_home.dart';
 import 'package:proodonto/app/pages/exam/exam_home.dart';
 import 'package:proodonto/app/pages/home/patient_item_list.dart';
+import 'package:proodonto/app/pages/patientInformations/patient_informations.dart';
 import 'package:proodonto/app/pages/triage/triage_home.dart';
 import 'package:proodonto/app/shared/default_size.dart';
 
 import '../patient/patient_home.dart';
 
 class HomePage extends StatelessWidget {
-  HomePage({Key? key, required this.database}) : super(key: key);
+  const HomePage({Key? key, required this.database}) : super(key: key);
   final ProodontoDatabase database;
 
   void _changeToPatientHomePage(BuildContext context) {
@@ -25,14 +26,18 @@ class HomePage extends StatelessWidget {
     Navigator.push(
         context,
         MaterialPageRoute(
-            builder: (context) => TriageHome(database: database,)));
+            builder: (context) => TriageHome(
+                  database: database,
+                )));
   }
 
   void _changeToExamHomePage(BuildContext context) {
     Navigator.push(
         context,
         MaterialPageRoute(
-            builder: (context) => ExamHome(database: database,)));
+            builder: (context) => ExamHome(
+                  database: database,
+                )));
   }
 
   void _changeToAnamnesisHome(BuildContext context) {
@@ -42,75 +47,78 @@ class HomePage extends StatelessWidget {
             builder: (context) => AnamnesisHome(database: database)));
   }
 
+  void _changeToPatientInformation(BuildContext context) {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => const PatientInformation()));
+  }
+
   @override
   Widget build(BuildContext context) {
     final key = GlobalObjectKey<ExpandableFabState>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        title: const Text("Home"),
       ),
       floatingActionButtonLocation: ExpandableFab.location,
       floatingActionButton: ExpandableFab(
         key: key,
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
         type: ExpandableFabType.up,
         distance: 50.0,
         expandedFabSize: ExpandableFabSize.regular,
-        childrenOffset: Offset(4, 30),
+        childrenOffset: const Offset(4, 30),
         expandedFabShape:
             RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
         collapsedFabShape:
             RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
         overlayStyle: ExpandableFabOverlayStyle(blur: 2),
         children: [
-          SizedBox(
-              width: 120,
-              height: 40,
-              child: ElevatedButton(
-                  onPressed: () => _changeToPatientHomePage(context),
-                  child: Text("Paciêntes"))),
-          SizedBox(
-              width: 120,
-              height: 40,
-              child: ElevatedButton(
-                  onPressed: () => _changeToTriageHomePage(context),
-                  child: Text("Triagem"))),
-          SizedBox(
-              width: 120,
-              height: 40,
-              child: ElevatedButton(
-                  onPressed: () => _changeToExamHomePage(context),
-                  child: Text("Exames"))),
-          SizedBox(
-              width: 120,
-              height: 40,
-              child: ElevatedButton(
-                  onPressed: () => _changeToAnamnesisHome(context),
-                  child: Text("Anamnese"))),
+          FloatingSubButton(title: "Paciêntes", navigate: () => _changeToPatientHomePage(context)),
+          FloatingSubButton(title: "Triagem", navigate: () => _changeToTriageHomePage(context)),
+          FloatingSubButton(title: "Exames", navigate: () => _changeToExamHomePage(context)),
+          FloatingSubButton(title: "Aamnese", navigate: () => _changeToAnamnesisHome(context)),
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.all(PaddingSize.small),
+        padding: const EdgeInsets.symmetric(
+            vertical: PaddingSize.small, horizontal: PaddingSize.medium),
         child: FutureBuilder(
-            future: database.patientDao.getAll(),
-            builder: (context, snapshot) {
-              return snapshot.hasData
-                  ? ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 4),
-                            child: PatientItem(
-                              patient: snapshot.data![index],
-                              onTap: (patient) {
-                                debugPrint(patient.name.toString());
-                            },
-                            ));
-                      })
-                  : const Center(child: Text("Vazia."));
-            },
-          ),
+          future: database.patientDao.getAll(),
+          builder: (context, snapshot) {
+            return snapshot.hasData
+                ? ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 4),
+                          child: PatientItem(
+                            patient: snapshot.data![index],
+                            onTap: (patient) => _changeToPatientInformation(context),
+                          ));
+                    })
+                : const Center(child: Text("Vazia."));
+          },
         ),
+      ),
     );
   }
 }
+
+class FloatingSubButton extends StatelessWidget {
+  const FloatingSubButton({super.key, required this.title, required this.navigate});
+  final String title;
+  final Function() navigate;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: 120,
+        height: 40,
+        child: ElevatedButton(
+            onPressed: () => navigate(),
+            child: Text(title)
+        )
+    );
+  }
+}
+
---
+++ proodonto/lib/app/pages/patientInformations/patient_informations.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PatientInformation extends StatefulWidget {
+  const PatientInformation({super.key});
+
+  @override
+  State<PatientInformation> createState() => _PatientInformationState();
+}
+
+class _PatientInformationState extends State<PatientInformation> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
