2d196be10f3fc56f7e4c27f032460e77679218ee
Eduardo <ceduardolima2000@gmail.com>
2023-08-11 10:25:03 -0300

    bugfix: resolvendo bug de formatação de data

--- proodonto/lib/app/shared/form_field_date_time.dart
+++ proodonto/lib/app/shared/form_field_date_time.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:intl/intl.dart';
 import 'package:proodonto/app/shared/default_size.dart';
 
 class FormFieldDateTime extends StatelessWidget {
@@ -10,19 +11,26 @@ class FormFieldDateTime extends StatelessWidget {
   final String label;
   String name;
 
+  DateTime parse(String date) {
+    DateTime dateTime = DateTime.parse(date);
+    String formattedString = DateFormat("dd/MM/yyyy").format(dateTime);
+    return DateFormat("dd/MM/yyyy").parse(formattedString);
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: PaddingSize.small),
       child: FormBuilderDateTimePicker(
         name: name,
+        format: DateFormat("dd/MM/yyyy"),
         decoration: InputDecoration(
             labelText:  label,
             helperText: "",
             suffixIcon: const Icon(Icons.date_range)),
         inputType: InputType.date,
         initialDate:
-            initialDate == null ? null : DateTime.tryParse(initialDate!),
+            initialDate == null ? null : DateFormat("dd/MM/yyyy").parse(initialDate!),
       ),
     );
   }
