9ef9a04e7a2b118c69d333cc6eaf91b7c1c078f4
Eduardo <ceduardolima2000@gmail.com>
2023-08-11 09:27:27 -0300

    feature: Adicionando suporte a localidades (BR)

--- proodonto/lib/app/pages/main/proodonto.dart
+++ proodonto/lib/app/pages/main/proodonto.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:proodonto/app/database/database.dart';
 import 'package:proodonto/app/pages/login/login.dart';
 import 'package:proodonto/app/theme/main_theme.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,6 +19,15 @@ class Proodonto extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      supportedLocales: const [
+        Locale('pt', 'BR')
+      ],
+      locale: const Locale('pt', 'BR'),
       home: LoginPage(database: database),
       theme: ThemeData(
         inputDecorationTheme: MainInputTheme().theme()
